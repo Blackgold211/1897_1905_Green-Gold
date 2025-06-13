625de74698f2f77016634a1af0e5eb2e06c18d52
CAHAT0002 <CAHAT0002@gmail.com>
2025-06-14 01:52:53 +1000

    2.22

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "The Green and Gold: An Alternative Alternate History" + '_' + "A Mod of Autumn Chen's Red Autumn SPD";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
